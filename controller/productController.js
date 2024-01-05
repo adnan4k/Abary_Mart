6b38aba0ddb6eb3d72fd8333af6e4516a5a60243
adnan4k <fayomuhe5@gmail.com>
2024-01-05 09:54:39 +0300

    update product added

--- controller/productController.js
+++ controller/productController.js
@@ -30,3 +30,30 @@ export const createProduct = async (req,res)=>{
         console.log(error)
     }
 }
+
+export const updateProduct = async(req,res,next) =>{
+    const { 
+          
+        title,
+        image,
+        description,
+    } = req.body;
+    const {id} = req.params;
+    
+
+    try {
+    const product = await Product.findByIdAndUpdate(id,{
+        title:title,
+        image:image,
+       description:description
+    })    
+       const savedProduct = await product.save();
+    if(!savedProduct){
+     return res.status(500).json({message:"error while saving"});
+    }
+
+    return res.status(200).json(savedProduct)
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
--- route/productRoute.js
+++ route/productRoute.js
@@ -1,9 +1,10 @@
 import express  from "express"
-import { createProduct } from "../controller/productController.js";
+import { createProduct, updateProduct } from "../controller/productController.js";
 
 const productRoutes = express.Router();
 
 productRoutes.post('/create-product',createProduct);
+productRoutes.put('/update-product/:id',updateProduct);
 
 
 export default productRoutes;
