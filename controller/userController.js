734fa243bdbc77a805a71b09789295460a916d82
adnan4k <fayomuhe5@gmail.com>
2024-02-22 20:35:25 +0300

    authentication is done

--- controller/userController.js
+++ controller/userController.js
@@ -16,18 +16,21 @@ export const addUser = async(req,res) =>{
         const savedUser = await user.save();
 
         if(!savedUser){
-         return res.status(500).json({message:"error while saving"});
+         return res.redirect('/user/signup');
 
         }
 
-        return res.status(200).json(savedUser)
+        return res.redirect('/product/view-product')
     } catch (error) {
         return res.status(500).json({message:error.message})
     }
 }
 
 export const viewLogin = async(req,res) =>{
-    return res.render('login',{login:"login"});
+    return res.render('login',{title:"Login",myRoute:'/user/login'});
+}
+export const signup = async(req,res) =>{
+    return res.render('signup',{title:"Signup",myRoute:'/user/singup'});
 }
 export const login = async(req,res) =>{
     
@@ -40,37 +43,19 @@ export const login = async(req,res) =>{
         let passwordMatch
         if(user){
 
-             passwordMatch = await bcrypt.compare(password, user.password);
+             passwordMatch =  bcrypt.compare(password, user.password);
         }
 
         if (passwordMatch) {
             req.session.userId = user.id;
             
             // return res.render(200).json({message:"successfull login",userId:user.id})
-            return res.render('login',{userId:user.id});
+            return  res.redirect('/product/view-product');
 
         } else {
            return res.render('login',{failru:"Invalid credentials",
            });
         }
    
-        async function sendEmailToAdminForContact(user, email) {
-            const transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                    user: "fayomuhe5@gmail.com",
-                    pass: "vypd cqxp eqqm krsg",
-                    port: 465,
-                    secure: true,
-                }
-            });
         
-            let info = await transporter.sendMail({
-                from: email,
-                to: "fayomuhe5@gmail.com",
-                subject: "New message arrived ",
-                text: `a New message has arrived: ${JSON.stringify(user)}`,
-                html: `<b>a New message has arrived:</b> <pre>${JSON.stringify(user, null, 2)}</pre>`,
-            });
-        }
 }
